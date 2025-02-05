aabd7ffd11d74359326828f5a762ce4c3111fc78
DJCooper1104 <dj.cooper1104@gmail.com>
2025-02-05 15:16:40 -0500

    Configured AWS Amplify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["react-router-dom"], // Ensure Vite doesn't try to bundle it incorrectly
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"], // Ensure Vite doesn't try to bundle it incorrectly
-    },
-  },
+  base: '/', // Adjust if needed for subdirectories
 });
